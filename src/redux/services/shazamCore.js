ff14176a42cfc88835b4526c2956013ed3dc8712
Akmal Adnan <akmal.a@okwave.global>
2023-02-11 23:46:50 +0800

    initial commit

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', 'Key Here');
+
+      return headers;
+    },
+  }),
+
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
