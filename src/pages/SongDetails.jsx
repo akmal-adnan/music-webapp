4067b1b0982f4d080b59c20d6d880cc5d9b96b56
Akmal Adnan <akmal.a@okwave.global>
2023-02-16 02:53:43 +0800

    update

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -29,7 +29,7 @@ const SongDetails = () => {
     dispatch(playPause(false));
   };
 
-  const handlePlayClick = () => {
+  const handlePlayClick = (song, i) => {
     dispatch(setActiveSong({ song, data, i }));
     dispatch(playPause(true));
   };
